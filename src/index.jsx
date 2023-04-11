f728f1835cc8cfd6ef7c1504788d7521a358d70f
SurajSinghGalaxy <surajsingh.parihar@galaxyweblinks.in>
2023-04-11 11:25:56 +0530

    Add font css in src folder

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
+import "./font.css";
 import "./App.css";
 import "./responsive.css";
 import "bootstrap/dist/js/bootstrap.js";
